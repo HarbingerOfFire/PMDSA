ed38858d37eb1c8c23f3eca51620d59e077bcea5
HarbingerOfFire <HarbingerOfFire@proton.me>
2025-03-26 13:15:07 -0600

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,27 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Python script to execute
+set "PYTHON_SCRIPT=main.py"
+
+:: Print header
+echo Index,Filename,Separation,Angle,Dmag
+
+:: Initialize counter
+set COUNT=0
+
+:: Loop through all files in the specified directory
+for %%F in ("%~1\*") do (
+    set "FILENAME=%%~nxF"
+    
+    :: Execute the Python script with the file as an argument and capture the output
+    for /f "delims=" %%O in ('python "%PYTHON_SCRIPT%" "%%F"') do set "OUTPUT=%%O"
+
+    :: Print the file number, filename, and Python output separated by commas
+    echo !COUNT!,!FILENAME!,!OUTPUT!
+
+    :: Increment counter
+    set /a COUNT+=1
+)
+
+endlocal
